f29e0e0081fccdc2c3b7b704451aff67913ebe7a
Dev-Moynul <stu.moynulislam@gmail.com>
2025-11-29 17:38:36 +0600

    fgets function

--- main.c
+++ main.c
@@ -3,8 +3,15 @@
 
 int main()
 {
-   char name[20];
-   fgets(name,sizeof(name),stdin);
-   printf("My name is %s",name);
+    char name[20], age[5];
+
+    printf("Enter your name: ");
+    fgets(name, sizeof(name),stdin);
+
+    printf("Enter your age: ");
+    fgets(age, sizeof(age),stdin);
+
+    printf("My name is %s and I am %d years old", name, age);
+
     return 0;
 }
